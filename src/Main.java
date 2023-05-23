f463ac1d98901d00b12fa68b2279554a26c54b30
ppquokka <quokka2335@gmail.com>
2023-05-23 13:13:37 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Jiwon Kim";
+        System.out.println(name);
+      //return 0;  
+    }
+}
---
+++ src/Prints.java
@@ -0,0 +1,7 @@
+public class Prints {
+    public static void main(String[] args) {
+        String name = "빅데이터 java 교육과정";
+        System.out.println(name);
+    }
+}  
+
