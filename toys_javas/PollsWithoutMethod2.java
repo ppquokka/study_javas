9d4b28f0634292eabd49ff929a1aeddd48a232d0
ppquokka <quokka2335@gmail.com>
2023-05-25 18:23:05 +0900

    230525   18:22

--- toys_javas/PollsWithoutMethod2.java
+++ toys_javas/PollsWithoutMethod2.java
@@ -2,34 +2,74 @@ import java.util.Scanner;
 
 public class PollsWithoutMethod2 {
     public static void main(String[] args) {
+        System.out.println(">이름을 입력하세요");
+        // 입력 준비
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String sun = "이름)";
+        String moon = "";
+        moon = myObj.nextLine();
+        System.out.println(sun+moon);
+       
         String answer = "";
-        String[] answers = {"", ""};
-        String[][] polls = {
-            {"1. 문항"},
-            {"(1) 답항", "(2) 답항", "(3) 답항", "(4) 답항"},
-            {"2. 문항"},
-            {"(1) 답항", "(2) 답항", "(3) 답항", "(4) 답항"},
-            {"3. 문항"},
-            {"(1) 답항", "(2) 답항", "(3) 답항", "(4) 답항"},
-            {"4. 문항"},
-            {"(1) 답항", "(2) 답항", "(3) 답항", "(4) 답항"},
-        };
+        String[0] answers = {"", "", "", ""};
 
-        int count = 0;
-        for (int second=0; second < polls.length; second=second+2){
-            System.out.println(polls[second][0]);
-            // 답항 출력
-            System.out.print("답하기 : ");
-            answers[count] = myObj.nextLine();
-            count = count + 1;
-            System.out.println();
-        }
+        System.out.println("1. 문항");
+        System.out.println("(1)답항" + "(2)답항" + "(3)답항" + "(4)답항");
+        System.out.print("답하기 : ");
+        answers[0] = myObj.nextLine();
+        
+        System.out.println("2. 문항");
+        System.out.println("(1)답항" + "(2)답항" + "(3)답항" + "(4)답항");
+        System.out.print("답하기 : ");
+        answers[1] = myObj.nextLine();
+
+        System.out.println("3. 문항");
+        System.out.println("(1)답항" + "(2)답항" + "(3)답항" + "(4)답항");
+        System.out.print("답하기 : ");
+        answers[2] = myObj.nextLine();
+
+        System.out.println("4. 문항");
+        System.out.println("(1)답항" + "(2)답항" + "(3)답항" + "(4)답항");
+        System.out.print("답하기 : ");
+        answers[3] = myObj.nextLine();
 
         for (int first=0; first < answers.length; first=first+1) {
             System.out.print(answers[first]+", ");
-        }
-        System.out.println();
+        break;
+        System.out.println();}
         // return 0;
     }
 }
+
+// import java.util.Scanner; 못하겠다
+// Scanner myObj = new Scanner(System.in); // Create a Scanner object
+// String answer = "";
+// String[] answers = {"", ""};
+// String[][] polls = {
+// {"1. 문항"},
+// {"(1) 답항", "(2) 답항", "(3) 답항", "(4) 답항"},
+// {"2. 문항"},
+// {"(1) 답항", "(2) 답항", "(3) 답항", "(4) 답항"},
+// {"3. 문항"},
+// {"(1) 답항", "(2) 답항", "(3) 답항", "(4) 답항"},
+// {"4. 문항"},
+// {"(1) 답항", "(2) 답항", "(3) 답항", "(4) 답항"},
+// };
+
+// int count = 0;
+// for (int second=0; second <= polls.length; second=second+2){
+// System.out.println(polls[second][0]);
+// System.out.print("답하기 : ");
+// answers[count] = myObj.nextLine();
+// count = count + 1;
+// System.out.println();
+// }
+
+// for (int first=0; first < answers.length; first=first+1) {
+// System.out.print(answers[first]+", ");
+// }
+
+// System.out.println();
+// // return 0;
+// }
+// }
