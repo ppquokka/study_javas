cc1850c6fc30f647ed80296452c5e37f37081a5a
ppquokka <quokka2335@gmail.com>
2023-05-25 10:31:25 +0900

    230525  10:31 실습 수정

--- src/LoopWithMark.java
+++ src/LoopWithMark.java
@@ -3,8 +3,8 @@ public class LoopWithMark {
         // for문만 사용. while문 사용 불가
         // 1~5번째 줄에 줄숫자만큼 ?표시
         int second = 5;        
-        for (int first = 1; first < second; first = first + 1;){
-            System.out.println(for +", " + first);
+        for (int first = 1; first < second; first = first + 1){
+            System.out.println("for" +", " + first);
         }
         
         System.out.println();
