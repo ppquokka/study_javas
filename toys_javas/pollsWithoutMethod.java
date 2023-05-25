cef5f5fa6cae49608369ebccc72be4c82073da77
ppquokka <quokka2335@gmail.com>
2023-05-25 15:42:09 +0900

    230525(목)  15:41  실습...ing

---
+++ toys_javas/pollsWithoutMethod.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class pollsWithoutMethod{
+    public static void main(String[] args) {
+        System.out.println(">이름을 입력하세요");
+        // 입력 준비
+        Scanner myObj = new Scanner(System.in);
+        String sun = "이름)";
+        String moon = "";
+        moon = myObj.nextLine();
+        System.out.println(sun+moon);
+           String[][] Question = {
+                                   {"1. 교수는 수업 전 강의 목표를 명확히 제시하였습니까?"},
+                                   {"①전혀 아니다","②아니다","③그렇다","④매우 그렇다"}
+                                  };
+                        for (int first=0; first < Question.length; first = first + 1){
+                            for (int second=0; second < Question[first].length; second = first + 1){
+                                System.out.println(Question[first][second]); 
+                            }  
+                        }           
+                        System.out.println();
+
+    }
+}
+// return 0;
