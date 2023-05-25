df43c64c33073197db3b490dc266e390ff04291e
ppquokka <quokka2335@gmail.com>
2023-05-25 17:07:11 +0900

    23.05.25.   17:06   강사님 풀이 최종

--- src/PollsWithoutMethod.java
+++ src/PollsWithoutMethod.java
@@ -6,16 +6,39 @@ public class PollsWithoutMethod {
         String answer = "";
         // ↓ 변수를 묶는다 = 움직이기 쉽다 (예: 숫자의 묶음이네! 문자의 묶음이네!)
         String[] answers = {"", ""};
-        
-        System.out.println("1. 문항");
-        System.out.println("(1) 답항"+"(2) 답항"+"(3) 답항"+"(4) 답항");
-        System.out.print("답하기 : ");
-        answers[0] = myObj.nextLine();   
+        // ↓ 반복문을 돌리겠다(문제3,4). 먼저 값을 묶음으로 묶는다.
+        String[][] polls = {
+            {"1. 문항"}, 
+            {"(1) 답항","(2) 답항","(3) 답항","(4) 답항"},
+            {"2. 문항"}, 
+            {"(1) 답항","(2) 답항","(3) 답항","(4) 답항"},
+        };
+        // 내부에서 사용할 변수값  
+        int count = 0;
+        // 문항만 찍는다
+        for (int second=0; second < polls.length; second = second +2){
+            System.out.println(polls[second][0]);
+            // 답항 출력
+             System.out.print("답하기 : ");
+             answers[count] = myObj.nextLine();
+             count = count + 1;
+            System.out.println();
+        } 
 
-        System.out.println("2. 문항");
-        System.out.println("(1) 답항"+"(2) 답항"+"(3) 답항"+"(4) 답항");
-        System.out.print("답하기 : ");
-        answers[1] = myObj.nextLine(); 
+        // System.out.println("1. 문항");
+        // System.out.println("(1) 답항"+"(2) 답항"+"(3) 답항"+"(4) 답항");
+        // System.out.print("답하기 : ");
+        // answers[0] = myObj.nextLine();   
+
+        // System.out.println("2. 문항");
+        // System.out.println("(1) 답항"+"(2) 답항"+"(3) 답항"+"(4) 답항");
+        // System.out.print("답하기 : ");
+        // answers[1] = myObj.nextLine(); 
+
+        // // 반복이 된다는 건 while이나 for문 사용 가능하다.그럼 데이터를 넣고 
+
+        // System.out.println("---------------------설문 종료---------------------");
+        // System.out.println("---------------------설문 결과---------------------");
 
         for (int first=0; first < answers.length; first = first + 1 ){
            System.out.print(answers[first]+",");
