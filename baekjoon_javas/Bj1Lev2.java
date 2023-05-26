51deef694b1299d44771f8b806bce20da30b19e7
ppquokka <quokka2335@gmail.com>
2023-05-26 09:48:22 +0900

    230526  09:48

--- baekjoon_javas/Bj1Lev2.java
+++ baekjoon_javas/Bj1Lev2.java
@@ -1,6 +1,12 @@
 public class Bj1Lev2 {
-    // 1000번. A+B.
-    public static void main(String[] args) {
-        System.out.print(1+2); 
+    public class Main {
+        public static void main(String[] args) {
+          System.out.println("Hello World");
+        }
     }
 }
+//     // 1000번. A+B.
+//     public static void main(String[] args) {
+//         System.out.println(12); 
+//     }
+// }
