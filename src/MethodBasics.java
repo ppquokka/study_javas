e7f155ce85412f426394d32347dd238fec74ca67
ppquokka <quokka2335@gmail.com>
2023-05-30 14:50:16 +0900

    230530   14:49

---
+++ src/MethodBasics.java
@@ -0,0 +1,53 @@
+public class MethodBasics {
+    /*  // class는 function의 모임. 'public static' 펑션과 동급으로 만들어야 함
+    <access_modifier> <return_type> <method_name>( list_of_parameters)
+    {
+    //body
+    }*/
+
+    // f(x) = x + 1  → 숫자의 결과로 나옴  | 결국 f(x) → 숫자결과
+    // x에 +1 
+    /*public int F(int x)   // 들어오는 data type은 return전에 들어감
+    {                                    
+        int result = x + 1;
+        return result;   //return은 문자열, 묶음 등의 결과값을 브레이스{}밖으로 던져줌
+                         //java는 data type이 중요하다. 'return_data_type'은 result와 맞춰야 함   
+    }
+    public static void main(String[] args) {
+        try {
+            int x = 4;
+            int y = 0;
+            // y = x + 1;   //변수에 +1   → f(x) = x + 1
+            //y = F(x)      //컴퓨터의 하드웨어에 실려야 실행이 됨. instance가 되었다 = "LAM(memory)"에 올라와야 한다.
+                          //실행 = CPU가 해석했다.   
+                          //얘는 public static 영역안에 들어있고, F(x)는 위의 public int 영역안에 있음.그래서 실행이 안됨.
+                          //주로 class를 instance화 한다. function은 내부에서 알 수없으므로, 밖의 전체를 가져온다.
+            MethodBasics methodBasics = new MethodBasics();   //instance화 
+            y = methodBasics.F(x); //class내의 function이다. 펑션은 리턴(결과값)을 냄.
+                                        //여러개 functiond의 class를 변수화 시킴. class를 function화 시킴. class도 변수 될수 있다.
+                                        // 얘가 다 담고있음. 변수는 아주 적은 것~엄청 큰 것 다 담을 수있음
+                                        //class도 변수에 담을 수 있다. data type을 굉장이 중요하게 관리함
+                                        //'methodBasics'의 변수 type은 class다.  ∴ class 자체가 data type이 된다
+                                        // class에 function넣는 이유: 데이터를 얼마나 잘 가공해서 던져 줄 수 있는가
+                                        //class는 data type이다. 
+            y = y + 1;   //변수에 +1
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        System.out.println();
+        // return 0;
+    }*/
+    public int f(int y) {
+        int end = y + 1;
+        return end;
+
+    }
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+}
