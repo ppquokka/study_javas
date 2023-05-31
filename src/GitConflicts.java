e0d9ed3b7e2976317cb923d2f49942eafaeed24f
ppquokka <quokka2335@gmail.com>
2023-05-31 11:42:36 +0900

    B개발자 커밋

---
+++ src/GitConflicts.java
@@ -0,0 +1,12 @@
+public class GitConflicts {
+    //  B 개발자 (Develops/anotherme/study_javas)
+    public static void main(String[] args) {
+        try {
+            int first = 2;
+            first = first + 1;
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // return 0;
+    }
+}
