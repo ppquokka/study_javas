08a2d7907ebcd2281051af7b31fa026524ff6e5a
ppquokka <quokka2335@gmail.com>
2023-06-13 17:21:22 +0900

    230613     SQL 팀플

---
+++ src/commons/commons.java
@@ -0,0 +1,14 @@
+package commons;
+
+import java.util.UUID;
+
+public class commons {
+    public String generateUUID() {
+        return UUID.randomUUID().toString();
+    }
+    
+}
+
+/*
+ * UUID 유니트 아이디 생성
+ */
---
+++ src/toys_javas/src/PollsWithMethod/PollsWithmethod.java
@@ -0,0 +1,12 @@
+package toys_javas.src.PollsWithMethod;
+
+public class PollsWithmethod {
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+}
